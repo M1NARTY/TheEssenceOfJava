ec80e731d8692448070af6ce3069bfe4d254cdaa
m1narty <jsjake4770@gmail.com>
2025-01-15 14:28:36 +0000

    짧막한 수정

--- src/Exercise4_6.java
+++ src/Exercise4_6.java
@@ -1,7 +1,7 @@
 // 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 프로그램
 //public class Exercise4_6 {
 //    public static void main(String[] args) {
-//        // 어케 함? 나 밥 다 됐네...
+//        // 어케 함? 나 퇴물 다 됐네...
 //        int dice1, dice2;
 //        int i = 0;
 //        while (i < 35) {
@@ -21,7 +21,7 @@ public class Exercise4_6 {
         int dice1, dice2;
 
         for (int i=1; i<=6; i++) {
-            for(int j=1; j<6; j++) {
+            for(int j=1; j<=6; j++) {
                 dice1 = i;
                 dice2 = j;
                 if(dice1 + dice2 == 6) {
